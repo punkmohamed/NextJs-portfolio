666ed24fc6b4b2c06dd6d0942ccf1a9deb1e7507
punkmohamed <punkmohamed22@gmail.com>
2024-08-28 00:15:34 +0200

    first commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,21 +3,21 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 const Homepage = () => {
   return (
-  <motion.div className="h-full" initial={{y:"-200vh"}} animate={{y:"0%"}} transition={{duration:1}}>
+    <motion.div className="h-full" initial={{ y: "-200vh" }} animate={{ y: "0%" }} transition={{ duration: 1 }}>
       <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
-      <div className=" h-1/2 lg:h-full lg:w-1/2 relative">
-        <Image src="/hero.png" alt="" fill className="object-contain" />
-      </div>
-      <div className="h-1/2 lg:h-full lg:w-1/2  flex flex-col gap-8 justify-center items-center">
-        <h1 className="font-bold text-4xl md:text-6xl">Lorem ipsum dolor sit amet </h1>
-        <p className="md:text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, sequi, repellat tempora earum ut doloremque excepturi ipsa reprehenderit sapiente molestiae laboriosam rerum vero quo recusandae! At quisquam aliquam nisi atque.</p>
-        <div className="w-full flex gap-4">
-          <button className="bg-black text-white p-5 rounded-lg ring-1 ring-white">View My Work</button>
-          <button className="bg-white text-black p-5 rounded-lg ring-1 ring-black">Contact Me</button>
+        <div className=" h-1/2 lg:h-full lg:w-1/2 relative">
+          <img src="/hero.png" alt="" fill className="object-contain" />
+        </div>
+        <div className="h-1/2 lg:h-full lg:w-1/2  flex flex-col gap-8 justify-center items-center">
+          <h1 className="font-bold text-4xl md:text-6xl">Lorem ipsum dolor sit amet </h1>
+          <p className="md:text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, sequi, repellat tempora earum ut doloremque excepturi ipsa reprehenderit sapiente molestiae laboriosam rerum vero quo recusandae! At quisquam aliquam nisi atque.</p>
+          <div className="w-full flex gap-4">
+            <button className="bg-black text-white p-5 rounded-lg ring-1 ring-white">View My Work</button>
+            <button className="bg-white text-black p-5 rounded-lg ring-1 ring-black">Contact Me</button>
+          </div>
         </div>
       </div>
-    </div>
-  </motion.div>
+    </motion.div>
   )
 };
 

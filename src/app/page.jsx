4935d8361211f5cb94d4ddef8fa4dc53eb5cc080
punkmohamed <punkmohamed22@gmail.com>
2024-08-28 00:34:08 +0200

    first commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 "use client"
 import Image from "next/image";
 import { motion } from "framer-motion";
+import { Link } from 'next/link';
 const Homepage = () => {
   return (
     <motion.div className="h-full" initial={{ y: "-200vh" }} animate={{ y: "0%" }} transition={{ duration: 1 }}>
@@ -12,8 +13,9 @@ const Homepage = () => {
           <h1 className="font-bold text-4xl md:text-6xl">Lorem ipsum dolor sit amet </h1>
           <p className="md:text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, sequi, repellat tempora earum ut doloremque excepturi ipsa reprehenderit sapiente molestiae laboriosam rerum vero quo recusandae! At quisquam aliquam nisi atque.</p>
           <div className="w-full flex gap-4">
-            <button className="bg-black text-white p-5 rounded-lg ring-1 ring-white">View My Work</button>
-            <button className="bg-white text-black p-5 rounded-lg ring-1 ring-black">Contact Me</button>
+            <Link href="/portfolio"> <button className="bg-black text-white p-5 rounded-lg ring-1 ring-white">View My Work</button></Link>
+            <Link href="/contact"><button className="bg-white text-black p-5 rounded-lg ring-1 ring-black">Contact Me</button></Link>
+
           </div>
         </div>
       </div>
